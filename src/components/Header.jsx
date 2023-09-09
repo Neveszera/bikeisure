56d6e3e712b8809c5f432b3f37113aabcf9ef0e8
pagliacci <gianinisampaio1@gmail.com>
2023-09-08 22:10:40 -0300

    Cabecalho criado

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header() {
+  
+
+    return (
+      <>
+       <header>
+            <ul>
+                <li>Início</li>
+                <li>Processo</li>
+                <li>Planos</li>
+                <li>Contato</li>
+                <li>Entrar</li>
+            </ul>
+        </header>
+      </>
+    )
+  }
+  
+  export default Header
