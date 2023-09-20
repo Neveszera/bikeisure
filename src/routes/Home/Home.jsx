88fa331b8ae4ec68f94377880215a6241715095a
Neveszera <gabrielgng26@gmail.com>
2023-09-20 01:50:28 -0300

    Correção usabilidde carrosel e FAQ dinamico.

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -4,6 +4,7 @@ import Footer from '../../components/Footer';
 import { Link } from 'react-router-dom';
 import Ciclista from '../../assets/img/ciclista.png';
 import PlanosCarousel from '../../components/PlanosCarousel.jsx'
+import Faq from '../../components/FAQ.jsx'
 import './Home.css';
 
 export default function Home() {
@@ -27,22 +28,10 @@ export default function Home() {
       </section>
 
       <section className='planos'>
-        <h1>Nossos Planos</h1>
         <PlanosCarousel />
       </section>
 
-      <section className='faq'>
-        <h1>Perguntas Frequentes</h1>
-        <div className='faq-item'>
-          <h3>Tem aceitação para bicicletas montadas/personalizadas?</h3>
-          <p>Sim, temos aceitação para bicicletas que tiverem seus componentes alterados ou personalizadas pelo próprio segurado, desde que em eventual sinistro, tenha comprovação da aquisição de todos os componentes. Sempre que houver uma nova alteração de peças que altere o valor total da bicicleta, um endosso deverá ser realizado no LMI (limite máximo de indenização) de contratação.</p>
-        </div>
-        <div className='faq-item'>
-          <h3>Posso contratar o seguro de bike para bicicleta usada?</h3>
-          <p>Aceitamos bicicletas usadas com até 8 anos de uso, e para bicicletas elétricas, até 3 anos de uso. Neste caso, sugerimos que a contratação ocorra pelo valor de mercado da bike usada.</p>
-        </div>
-        {/* Adicione mais perguntas frequentes conforme necessário */}
-      </section>
+      <Faq />
 
       <Footer />
     </>
